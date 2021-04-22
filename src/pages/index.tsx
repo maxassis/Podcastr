c3e2e72ca23b4707c20ff46a38d930a0bfc383b3
maxassis <max.assis@ymail.com>
2021-04-22 20:49:33 -0300

    instalado axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-import Head from "next/head";
 import React from "react";
-import { Header } from "../components/Header";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home() {
-  return <></>;
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get(
+    "episodes?_limit=12&_sort=published_at&_order=desc"
+  );
+
+  return {
+    props: {
+      episodes: data,
+    },
+  };
+};
