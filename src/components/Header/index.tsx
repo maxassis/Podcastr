e5dc62442d57977ea11f41b0a3010fef90a8bd20
maxassis <max.assis@ymail.com>
2021-04-24 19:41:04 -0300

    finalizando o projeto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,18 @@
-import format from "date-fns/format";
-import ptBr from "date-fns/locale/pt-BR";
-
-import styles from "./styles.module.scss";
-const currentDate = format(new Date(), "EEEEEE, d MMMM", {
-  locale: ptBr,
-});
+import styles from './styles.module.scss'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  })
+
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
-
-      <p>O melhor para você ouvir sempre</p>
+      <img src="/logo.svg" alt="Podcastr"/>
+      <p>O melhor para você ouvir, sempre</p>
 
       <span>{currentDate}</span>
     </header>
-  );
+  )
 }
